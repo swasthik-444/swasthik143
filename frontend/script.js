4801945158510720ac069303cb8d53fc89d58160
swasthik-444 <swasthiks444@gmail.com>
2026-03-25 10:30:39 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -10,7 +10,7 @@ document.getElementById('feedbackForm').addEventListener('submit', async (e) => 
     statusText.innerText = "Sending...";
 
     // PASTE YOUR RENDER LINK HERE
-    const API_URL = "https://my-portfolio-m5w5.onrender.com/api/feedback";
+    const API_URL = "https://swasthik143-2.onrender.com/api/feedback";
 
     try {
         const response = await fetch(API_URL, {
